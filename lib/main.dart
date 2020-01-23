0d96b2bd3435d6422e9086433f98294cc312e084
rusaco <gonzalez-exequiel@hotmail.com>
2020-01-23 01:30:44 -0300

    Aplicación de la hora mundial. En esta primer version se agrego para poder pasar de una ventana a otra, y volver luego

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hora_mundial/pages/choose_location.dart';
+import 'package:hora_mundial/pages/home.dart';
+import 'package:hora_mundial/pages/loading.dart';
+
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+      },
+    ));
